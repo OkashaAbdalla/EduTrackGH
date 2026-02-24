77a5801c01a0c347d1d9d5d833853f103e52fc36
OkashaAbdalla <okashaabdalla44@gmail.com>
2026-02-24 15:21:52 +0100

    refactor: remove legacy session-based attendance system

--- eduTrackGH-frontend/src/pages/lecturer/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Lecturer Pages Barrel Export
- */
-
-export { default as LecturerDashboard } from './LecturerDashboard';
-export { default as CreateSession } from './CreateSession';
-export { default as ManageSessions } from './ManageSessions';
-export { default as ViewAttendance } from './ViewAttendance';
--- eduTrackGH-frontend/src/pages/student/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Student Pages Barrel Export
- */
-
-export { default as StudentDashboard } from './StudentDashboard';
-export { default as ActiveSessions } from './ActiveSessions';
-export { default as MarkAttendance } from './MarkAttendance';
-export { default as AttendanceHistory } from './AttendanceHistory';
